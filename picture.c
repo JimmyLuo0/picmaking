996bbe036c1625de75e182e20fea03a0bca50b78
JimmyLuo <jluo00@stuy.edu>
2020-01-31 22:23:17 -0500

    wrote heading

--- picture.c
+++ picture.c
@@ -1,7 +1,16 @@
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(){
   int fd;
-  open("image.ppm", O_CREAT|O_APPEND|O_EXCL, 0644);
-  
+  char * buf = "P3\n500 500\n255\n";
+  fd = open("image.ppm", O_CREAT|O_APPEND|O_RDWR|O_EXCL, 0644);
+  write(fd, buf, 15);
+  for(int i = 0; i < 500; i++){
+    for(int n = 0; n < 500; n++){
+
+    }
+  }
+  close(fd);
+  return 0;
 }
