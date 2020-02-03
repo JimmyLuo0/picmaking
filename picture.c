cdd30b75f654789d2937aa3de083f97f7c2d0b06
JimmyLuo <jluo00@stuy.edu>
2020-02-03 09:49:49 -0500

    missing a zero but somehow works

--- picture.c
+++ picture.c
@@ -1,16 +1,36 @@
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+int prime(int n){
+  int prime = 1;
+  for(int divide = 2; divide < n; divide++){
+    if(!(n%divide))
+      prime = 0;
+  }
+  if(n < 2)
+    return 0;
+  return prime;
+}
 int main(){
-  int fd;
-  char * buf = "P3\n500 500\n255\n";
-  fd = open("image.ppm", O_CREAT|O_APPEND|O_RDWR|O_EXCL, 0644);
-  write(fd, buf, 15);
-  for(int i = 0; i < 500; i++){
-    for(int n = 0; n < 500; n++){
-
+  // int fd;
+  // char * buf = "P3\n500 500\n255\n";
+  // fd = open("image.ppm", O_CREAT|O_APPEND|O_RDWR|O_EXCL, 0644);
+  // write(fd, buf, 15);
+  // close(fd);
+  FILE * fp;
+  fp = fopen("image.ppm", "w");
+  fprintf(fp, "P3\n500 500\n255\n");
+  int color = 0;
+  fp = fopen("image.ppm", "w");
+  for(int i = 1; i < 501; i++){
+    for(int n = 1; n <501; n++){
+      if(prime(n) && prime(i))
+        fprintf(fp, "%d %d %d\n ", 255, 255, 255);
+        else
+        fprintf(fp, "%d %d %d\n ", 0, 0, 0);
     }
   }
-  close(fd);
+  fclose(fp);
   return 0;
 }
